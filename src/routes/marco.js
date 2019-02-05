d26f385022d1c208ab9b418237b489ea673b006c
Oliver Coley <mxoliverleigh@gmail.com>
2019-02-05 13:44:57 -0500

    marco router test

--- src/routes/marco.js
+++ src/routes/marco.js
@@ -1,9 +1,5 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-
-router.get("/", (req, res, next) => {
-    res.send("Welcome to Bloccit");
-});
 
 router.get("/marco", (req, res, next) => {
     res.send("polo");
