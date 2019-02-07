d996883b0d1d160a5bc4c6da62ef1c63cadaa3c9
Oliver Coley <mxoliverleigh@gmail.com>
2019-02-07 14:11:36 -0500

    resource part 2 checkpoint complete

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -12,5 +12,60 @@ module.exports = {
                 res.render("topics/index", {topics});
             }
         })
+    },
+    new(req, res, next){
+        res.render("topics/new");
+    },
+    create(req, res, next){
+        let newTopic = {
+            title: req.body.title,
+            description: req.body.description
+        };
+        topicQueries.addTopic(newTopic, (error, topic) => {
+            if(error){
+                res.redirect(500, "/topics/new");
+            } else {
+                res.redirect(303, `/topics/${topic.id}`);
+            };
+        });
+    },
+    show(req, res, next){
+
+        topicQueries.getTopic(req.params.id, (error, topic) => {
+
+            if(error || topic == null){
+                res.redirect(404, "/");
+            } else {
+                res.render("topics/show", {topic});
+            }
+        });
+    },
+    destroy(req, res, next){
+        topicQueries.deleteTopic(req.params.id, (error, topic) => {
+            if(error){
+                res.redirect(500, `/topics/${topic.id}`)
+            } else {
+                res.redirect(303, "/topics")
+            }
+        });
+    },
+    edit(req, res, next){
+        topicQueries.getTopic(req.params.id, (error, topic) => {
+            if(error || topic == null){
+                res.redirect(404, "/");
+            } else {
+                res.render("topics/edit", {topic});
+            }
+        });
+    },
+    update(req, res, next){
+        topicQueries.updateTopic(req.params.id, req.body, (error, topic) => {
+
+            if(error || topic == null){
+                res.redirect(404, `/topics/${req.params.id}/edit`);
+            } else {
+                res.redirect(`/topics/${topic.id}`);
+            }
+        });
     }
 }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -11,6 +11,57 @@ module.exports = {
         .catch((error) => {
             callback(error);
         })
-    }
+    },
 
+    addTopic(newTopic, callback){
+        return Topic.create({
+            title: newTopic.title,
+            description: newTopic.description
+        })
+        .then((topic) => {
+            callback(null, topic);
+        })
+        .catch((error) => {
+            callback(error);
+        })
+    },
+
+    getTopic(id, callback){
+        return Topic.findById(id)
+        .then((topic) => {
+            callback(null, topic);
+        })
+        .catch((error) => {
+            callback(error);
+        })
+    },
+    deleteTopic(id, callback){
+        return Topic.destroy({
+            where: {id}
+        })
+        .then((topic) => {
+            callback(null, topic);
+        })
+        .catch((error) => {
+            callback(error);
+        })
+    },
+    updateTopic(id, updatedTopic, callback){
+        return Topic.findById(id)
+        .then((topic) => {
+            if(!topic){
+                return callback("Topic not found");
+            }
+
+            topic.update(updatedTopic, {
+                fields: Object.keys(updatedTopic)
+            })
+            .then(() => {
+                callback(null, topic);
+            })
+            .catch((error) => {
+                callback(error);
+            });
+        });
+    }
 }
